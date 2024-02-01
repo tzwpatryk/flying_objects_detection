6b7d8ab72e31ce2018c903d53770d2de2ab8260c
tzwpatryk <95307168+tzwpatryk@users.noreply.github.com>
2024-02-01 21:58:09 +0100

    Adding more information to readme and additional training

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Flying objects detection using YOLOv8
 
-This project is aimed at detecting flying objects such as drones, planes, birds, and helicopters using the state-of-the-art object detection algorithm YOLOv8. It also provides simple streamlit application with the possibility of upload your image, mp4 file or turn on your webcam.
+The project aims to detect flying objects such as drones, airplanes, birds and helicopters using the state-of-the-art YOLOv8 object detection algorithm. It also provides a simple streamlit application with the possibility of uploading an image, mp4 file or enabling a webcam.
+
+![Application interface](images/app.png)
+
+We used [datasetdrone-trv98_dataset](https://universe.roboflow.com/project-ddrone/datasetdrone-trv98/dataset/29) published on [Roboflow](https://universe.roboflow.com/) for training.
+
+![Augmented images from the training set](images/train_batch0.jpg)
+
+It is possible to make predictions on images, videos and your webcam.
+
+![Video prediction](images/vid_pred.png)
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from numpy import ndarray
+import numpy as np
 import streamlit as st
 import numpy as np
 from ultralytics import YOLO
@@ -6,9 +7,9 @@ import cv2
 from PIL import Image
 from streamlit_webrtc import webrtc_streamer
 import av
-from turn import get_ice_servers
 import tempfile
 
+# function that converts model prediction on an image to PIL Image
 def show_results(results, revert=True):
     im_array = results[0].plot()
     if revert:
@@ -17,27 +18,32 @@ def show_results(results, revert=True):
         im = Image.fromarray(im_array)
     return im
 
+# callback for a webcam that makes a prediction for each frame and returns image with boxes
 def video_frame_callback(frame):
     frame = frame.to_ndarray(format="bgr24")
+
     results = model(frame, conf=conf)
     im_array = results[0].plot()
-    img = av.VideoFrame.from_ndarray(im_array, format="bgr24")
 
+    img = av.VideoFrame.from_ndarray(im_array, format="bgr24")
     return img
 
-model = YOLO("runs/detect/train/weights/last.pt")
+# loading model with the best weights
+model = YOLO("runs/detect/train/weights/latest.pt")
 
-conf = st.slider("Confidence value", min_value=0.1, max_value=0.9, value=0.3)
-
+# adding streamlit components
+conf = st.slider("Confidence value", min_value=0.1, max_value=0.9, value=0.5)
 st.header("Prediction on image")
 image = st.file_uploader("Upload image")
 
+# predictions on images
 if image is not None:
     img_bytes = np.asarray(bytearray(image.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(img_bytes, 1)
 
     results = model(opencv_image, conf=conf)
     img = show_results(results)
+
     st.image(img)
 
 st.header("Prediction on video")
@@ -45,6 +51,7 @@ uploaded_video = st.file_uploader("Upload video", type="mp4")
 
 stframe = st.empty()
 
+# predictions on video
 if uploaded_video is not None:
 
     tfile = tempfile.NamedTemporaryFile(delete=False)
@@ -63,8 +70,8 @@ if uploaded_video is not None:
         frame_count += 1
     cap.release()
 
+# predictions on webcam
 st.header("Prediction on your webcam")
 webrtc_ctx = webrtc_streamer(key="object-detection-cam",
-                            rtc_configuration={"iceServers": get_ice_servers()},
                             media_stream_constraints={"video": True, "audio": False},
                             video_frame_callback=video_frame_callback)
